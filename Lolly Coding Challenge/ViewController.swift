fb5fba71c0384de3ca2e6b1c404b41f19456e1f9
Stanley <stanley.traub@gmail.com>
2021-01-11 19:33:06 -0800

    Inital commit

---
+++ Lolly Coding Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lolly Coding Challenge
+//
+//  Created by Stanley Traub on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
